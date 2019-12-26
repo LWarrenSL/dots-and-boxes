35ff8f1189be85b258a0013858c95fba57df5a47
Laura Warren <lwarren@scottlogic.com>
2019-12-26 19:55:15 +0000

    Code reworked following suggestions from Mark

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-function dotsAndBoxes(moves){
-    
-    let changePlayer = true;
+function dotsAndBoxes(moves) {
     const dimension = gridSizeDeterminer(moves);
     const boxIds = getPossibleBoxIdsForDimension(dimension);
 
-    const boxesThing = {};
+    const boxSides = {};
     for (let id of boxIds) {
-        boxesThing[id] = { top: false, bottom: false, left: false, right: false }
+        boxSides[id] = { top: false, bottom: false, left: false, right: false }
     }
 
     const state = {
         playersTurn: 1,
-        boxes: boxesThing,
+        boxes: boxSides,
         score: {
             "Player 1": 0,
             "Player 2": 0
@@ -24,182 +22,111 @@ function dotsAndBoxes(moves){
     return [state.score["Player 1"], state.score["Player 2"]];
 }
 
+function range(fromInclusive, toExclusive) {
+    const arr = new Array(toExclusive - fromInclusive);
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = i + fromInclusive;
+    }
+
+    return arr;
+}
+
 /**
  * Given a grid dimension, determines what the top left values of each of the potential boxes could be for this game
  * Returns an array. Numbers in the array are potential boxes contributed to by that line
  */
 function getPossibleBoxIdsForDimension(dimension) {
-    possibleBoxes = [];
-    let j;
-    for (j = 0; j < (dimension * dimension); j++) {
-        if (
-            !(
-                (
-                    (((dimension * dimension)-dimension) <= j) 
-                    && 
-                    (j < (dimension * dimension))
-                ) || (
-                    Number.isInteger((j + 1)/dimension)
-                )
+    return range(0, dimension * dimension)
+        .filter(j => !Number.isInteger((j + 1) / dimension)) // Exclude right column of grid
+        .filter(j => !(
+            (
+                (((dimension * dimension) - dimension) <= j) // Exclude bottom row of grid
             )
-        ){
-            possibleBoxes.push(j);
-        }
-    }
-    return possibleBoxes;
-};
+        ))
+}
 
 /**
  * Takes the input array and determines the grid size
  * e.g. returns 3 if grid is 3x3 dots
  */
 function gridSizeDeterminer(array) {
-    return (2+Math.sqrt((4+(8*array.length))))/4; 
+    return (2 + Math.sqrt((4 + (8 * array.length)))) / 4; 
 };
 
 /**
  * Takes a tuple and determines if the line is vertical or horizontal
  */
-function vertOrHorizChecker([a,b]) {
-    return Math.abs(a-b) === 1 ? "Horizontal" : "Vertical"
+function vertOrHorizChecker([a, b]) {
+    return Math.abs(a - b) === 1 ? "Horizontal" : "Vertical"
 };
 
 /**
  * Given a tuple and the grid dimension, determines what the top left values of each of the potential boxes could be for a given line input
- * Returns an array. Numbers in the array are potential boxes contributed to by that line
+ * Determines which sides correspond to each potential box
+ * Returns an array. Each element in the array is an object with two properties and corresponding values. These are the
+ * box number and the filled in side of that box
  */
-function determineContributionToWhichBoxes([a,b], dimension) {
-    switch (vertOrHorizChecker([a,b])) {
+function determineContributionToBoxes([a, b], dimension) {
+    const contributions = [];
+    switch (vertOrHorizChecker([a, b])) {
         case "Vertical":
-            return [
-                Number.isInteger((Math.min(a,b) + 1)/dimension)
-                    ?"empty"
-                    :Math.min(a,b), 
-                Number.isInteger(Math.min(a,b)/dimension) 
-                    ?"empty" 
-                    :(Math.min(a,b)-1)
-            ].filter(x => x !== "empty");
-
+            if (!Number.isInteger((Math.min(a, b) + 1) / dimension)) {
+                contributions.push({
+                    box: Math.min(a, b),
+                    side: "left"
+                })
+            }
+            if (!Number.isInteger(Math.min(a, b) / dimension)) {
+                contributions.push({
+                    box: Math.min(Math.min(a, b) - 1),
+                    side: "right"
+                })
+            } 
+            break;
         case "Horizontal":
-            return [
-                Math.min(a,b) >= dimension*(dimension - 1)
-                    ?"empty"
-                    :Math.min(a,b), 
-                Math.min(a,b) < dimension
-                    ?"empty" 
-                    :(Math.min(a,b)-dimension)
-            ].filter(x => x !== "empty");
+            if (Math.min(a, b) < dimension * (dimension - 1)) {
+                contributions.push({
+                    box: Math.min(a, b),
+                    side: "top"
+                })
+            }
+            if (Math.min(a, b) >= dimension) {
+                contributions.push({
+                    box: Math.min(a, b) - dimension,
+                    side: "bottom"
+                })
+            }
+            break;
     }
-};
-
-/**
- * Given a tuple line input and the grid dimension, determines which sides of the relevant boxes are filled in
- * Returns an array. Numbers in the array are sides constructed by that line
- */
-function determineContributionToWhichSides([a,b], dimension) {
-    switch (vertOrHorizChecker([a,b])) {
-        case "Vertical":
-            return [
-                Number.isInteger((Math.min(a,b) + 1)/dimension)
-                    ?"empty"
-                    :"left", 
-                Number.isInteger(Math.min(a,b)/dimension) 
-                    ?"empty" 
-                    :"right"
-            ].filter(x => x !== "empty");
-
-        case "Horizontal":
-            return [
-                Math.min(a,b) >= dimension*(dimension - 1)
-                    ?"empty"
-                    :"top", 
-                Math.min(a,b) < dimension
-                    ?"empty" 
-                    :"bottom"
-            ].filter(x => x !== "empty");
-    }
+    return contributions;
 };
 
 /**
  * Given the current state and a box id, returns true if that box has lines on all four sides
  */
-function isComplete(box, state) {
-    return state.boxes[box].top && state.boxes[box].left && state.boxes[box].right && state.boxes[box].bottom;
+function isComplete(box) {
+    return box.top && box.left && box.right && box.bottom;
 };
 
 /**
  * Given the current state, the game dimension and a tuple line input, updates the state based on the results of that line being played 
  */
-function updateState(tuple, dimension, state) {
-    let updatedBoxes = determineContributionToWhichBoxes(tuple, dimension);
-    let updatedSides = determineContributionToWhichSides(tuple, dimension);
+function updateState(move, dimension, state) {
+    let updatedBoxes = determineContributionToBoxes(move, dimension);
     let changePlayer = true;
+ 
+    for (let i = 0; i < updatedBoxes.length; i++) {
+        const boxId = updatedBoxes[i].box;
+        const box = state.boxes[boxId];
 
-    let i;    
-    for (i = 0; i < updatedBoxes.length; i++) {
-        state.boxes[updatedBoxes[i]][updatedSides[i]] = true;
-        if (isComplete(updatedBoxes[i], state)) {
+        box[updatedBoxes[i].side] = true;
+        if (isComplete(box)) {
             state.score["Player " + state.playersTurn] ++
             changePlayer = false;
         } 
     }
     if (changePlayer) {
         state.playersTurn = 3 - state.playersTurn;
-    }
-};
-
-function ignoreRandomStuff() {
-    const dimension = 4;
-    const boxesThing = {};
-    const boxIds = [0, 1, 2, 4, 5, 6, 8, 9, 10];
-
-    for (let id of boxIds) {
-        boxesThing[id] = { top: false, bottom: false, left: false, right: false }
-    }
-
-    const state = {
-        playersTurn: 1,
-        boxes: boxesThing,
-        score: {
-            "Player 1": 0,
-            "Player 2": 0
-        }
-    }
-
-    const id = "height"
-
-    const foo = {}
-    foo[id] = 165
-    foo.height = 165
-    foo["height"] = 165
-    foo[aFunction()] = 165
-
-
-    function aFunction() {
-        return "height"
-    }
-
-    const newFoo = {
-        height: 165,
-        likes: {
-            "coffee": 0.9,
-            "mushrooms": 0.3
-        }
-    }
-
-
-
-
-    function demonstratePlusplus() {
-        let foo = 0;
-
-        console.log(++foo)
-        console.log(foo)
-
-        const arr = [ 1, 2, 3 ]
-        let x = 0;
-        while (x < arr.length) {
-            arr[++x] *= 2;
-        }
     }
 }
